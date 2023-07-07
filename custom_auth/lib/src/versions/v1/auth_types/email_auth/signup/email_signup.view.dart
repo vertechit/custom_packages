051169086e3f41cfa31a6d5a392fbe18d136066b
wesley.mizerski <wesley.mizerski@conceptu.ind.br>
2023-07-07 07:59:35 -0300

    solved-cp-errors

--- custom_auth/lib/src/versions/v1/auth_types/email_auth/signup/email_signup.view.dart
+++ custom_auth/lib/src/versions/v1/auth_types/email_auth/signup/email_signup.view.dart
@@ -1,4 +1,5 @@
-import 'package:custom_app/custom_app/custom_app.imports.dart';
+import 'package:custom_app/no-arch/custom_app/out/app_controller/app.controller.interface.dart';
+import 'package:custom_app/no-arch/custom_app/out/view/custom_widget.view.dart';
 
 abstract class EmailSignupViewRequireds {
   //
